7fa227fe9a2c661f09dc6445f4ecfa4f7855b68a
Kyle O'Brien <kylewobrien@gmail.com>
2023-07-10 10:25:23 -0700

    Coursera project on React development - final version

--- todo_list/src/App.js
+++ todo_list/src/App.js
@@ -6,6 +6,23 @@ const App = () => {
   const [todoEditing, setTodoEditing] = React.useState(null);
   const [editingText, setEditingText] = React.useState("");
   
+  React.useEffect(() => {
+    const json = localStorage.getItem("todos");
+    const loadedTodos = JSON.parse(json);
+    if (loadedTodos) {
+      setTodos(loadedTodos);
+    }
+  }, []);
+  React.useEffect(() => {
+    if([todos].length > 0) {
+        const json = JSON.stringify(todos);
+        localStorage.setItem("todos", json);
+    }
+  }, [todos]);
+
+
+
+
   // Add the handlesubmit code here
   function handleSubmit(e) {
     e.preventDefault();
@@ -77,7 +94,7 @@ const App = () => {
                 <input
                   type="text"
                   onChange={(e) => setEditingText(e.target.value)}
-                />
+                  />
               ) : (
                 <div>{todo.text}</div>
               )}
